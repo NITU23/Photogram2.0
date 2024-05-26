1f9cb9e3422043e34804ae6779134016bf166b5d
Nitin vyas <nitinvyas200@gmail.com>
2024-05-26 20:41:00 +0530

    activity created

--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -1,11 +1,35 @@
-import img from '../images/superman.jpeg'
+import img from '../images/flower.jpeg'
 import '../css/card.css'
+import  {PiHeart} from 'react-icons/pi';
+import { FaHeart } from "react-icons/fa";
+import { FaRegComment } from "react-icons/fa";
+import { FiSend } from "react-icons/fi";
+import { useState } from 'react';
+import Name from './name'
 function Card() {
-    return (<div>
-        <div className='card'>
-            <img className='img' src={img} />
+    const [isClicked,setIsClicked] = useState(false);
+    const setClick =()=>{
+      isClicked===true ? setIsClicked(false):setIsClicked(true)
+    }
+    return (
+      <div>
+        <div className="card">
+            <div className='photoDetails'>
+            <Name />
+          <img className="img" src={img} />
+          <div className="actions img">
+            <div className='activity'>
+            {!isClicked  &&    < PiHeart className="like heart " onClick={setClick} /> }
+            {isClicked  &&    < FaHeart className="liked" onClick={setClick} /> }
+            <FaRegComment className="like" />
+            <FiSend className="like" />
+            </div>
+
+          </div>
+          </div>
         </div>
-    </div>);
+      </div>
+    );
 }
 
 export default Card;
---
+++ frontend/src/components/name.js
@@ -0,0 +1,16 @@
+import  '../css/name.css'
+
+
+function Name() {
+    return (
+    <div>
+      <div className='profile' >
+        <span className="nameUrl">NV</span>
+      <div className='name'>Nitin Vyas <br/>
+      <span className='subheading'>Hello</span></div>
+
+      </div>
+
+        </div>)
+}
+export default Name;
